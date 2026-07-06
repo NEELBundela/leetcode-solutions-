6beb2189a56e2a10ad1a5b371a3d8fb9071bf55f
Bundela Neel Dilipbhai <neelbundela1807@gmail.com>
2026-07-06 18:34:02 +0530

    Time: 45 ms (25.31%) | Memory: 47.1 MB (39.74%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int ans=nums[i]+nums[j];
+                if(ans==target){
+                   return new int[]{i,j};
+                }
+
+            }
+        }
+       return new int[]{};
+    }
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] arr={2,7,11,15};
+        int target=9;
+
+        s.twoSum(arr,target);
+    }
+}
