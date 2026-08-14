9b90470546cf1666b010eb551d9b608700647848
Bundela Neel Dilipbhai <neelbundela1807@gmail.com>
2026-08-14 20:43:49 +0530

    Time: 3 ms (52.38%) | Memory: 46.5 MB (43.84%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,42 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
-
         Set<Character>[] rows = new HashSet[9];
-        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] column = new HashSet[9];
         Set<Character>[] boxes = new HashSet[9];
 
-        for (int i = 0; i < 9; i++) {
+        for(int i = 0;i<9;i++){
             rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
+            column[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
         }
 
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
+        for(int row = 0;row<9;row++){
+            for(int col=0;col<9;col++){
 
                 char num = board[row][col];
 
-                if (num == '.') {
+                if(num == '.'){
                     continue;
                 }
 
-                int box = (row / 3) * 3 + (col / 3);
+                int box = (row/3)*3 + (col/3);
 
-                if (rows[row].contains(num) ||
-                    cols[col].contains(num) ||
-                    boxes[box].contains(num)) {
-
+                if(rows[row].contains(num) ||
+                   column[col].contains(num)||
+                   boxes[box].contains(num)){
                     return false;
-                }
-
-                rows[row].add(num);
-                cols[col].add(num);
-                boxes[box].add(num);
+                   }
+                   rows[row].add(num);
+                   column[col].add(num);
+                   boxes[box].add(num);
             }
         }
-
         return true;
     }
 }
-    
